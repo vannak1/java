06866a2afe66fc1e4b664129ddf888be590ef3a8
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-12 22:14:32 -0500

    added new calc project with arr, case and for each

---
+++ arrCalcEngine/src/arrCalcEngine/calcengine.java
@@ -0,0 +1,11 @@
+package arrCalcEngine;
+
+public class calcengine {
+
+	public static void main(String[] args) {
+		double[] rightVals = {100.0d, 25.0d, 225.0d, 11.0d};
+		double[] leftVals = {20.0d, 2.0d, 3.0d, 10.0d};
+	    
+	}
+
+}
