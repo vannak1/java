58f645fc456ceab5ef8383b89f3b791bd31d2dd1
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-12 22:32:32 -0500

    finished for each, for, arr, switch example

--- arrCalcEngine/src/arrCalcEngine/calcengine.java
+++ arrCalcEngine/src/arrCalcEngine/calcengine.java
@@ -4,8 +4,31 @@ public class calcengine {
 
 	public static void main(String[] args) {
 		double[] rightVals = {100.0d, 25.0d, 225.0d, 11.0d};
-		double[] leftVals = {20.0d, 2.0d, 3.0d, 10.0d};
-	    
+		double[] leftVals = {20.0d, 50.d, 3.0d, 10.0d};
+		char[] opCodes = {'d', 'a', 's', 'm'};
+		double[] results = new double[opCodes.length];
+		
+		for(int i = 0; i < opCodes.length; i++){
+	      switch(opCodes[i]) {
+	    	  case 'a':
+	            results[i] = leftVals[i] + rightVals[i];
+	            break;
+	      	  case 's':
+	            results[i] = leftVals[i] - rightVals[i];
+	            break;
+	      	  case 'd':
+	            results[i] = leftVals[i] / rightVals[i];
+	            break;
+	      	  case 'm':
+	            results[i] = leftVals[i] * rightVals[i];
+	            break;
+	      }
+		}
+		
+		for(double theResult : results){
+			System.out.print("result = ");
+			System.out.println(theResult);
+		}
 	}
 
 }
